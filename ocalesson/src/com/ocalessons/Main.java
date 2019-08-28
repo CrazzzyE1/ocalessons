58c919ede8eb3072fdfb55bd860ec5cb24e85473
CrazzzyE1 <litvakivan@gmail.com>
2019-08-28 10:29:55 +0300

    First lesson Home TASK

--- ocalesson/src/com/ocalessons/Main.java
+++ ocalesson/src/com/ocalessons/Main.java
@@ -5,8 +5,14 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-	// Course about OCA
-        System.out.println("The first Step to OCA! ");
+	// First HomeTask
+        System.out.println("               РУБАИ" +
+                "\nМного лет размышлял я над жизнью земной. " +
+                "\nНепонятного нет для меня под луной. " +
+                "\nМне известноб что мне ничего не известно!" +
+                "\nВотпоследняя правда, открытая мной. " +
+                "\n                               О.Хайям");
+
 
     }
 }
