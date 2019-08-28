74e1e0948ec6e8fa2810755e61f990b9f9594f86
CrazzzyE1 <litvakivan@gmail.com>
2019-08-28 10:09:28 +0300

    Start and first commit

---
+++ ocalesson/src/com/ocalessons/Main.java
@@ -0,0 +1,12 @@
+package com.ocalessons;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Course about OCA
+        System.out.println("The first Step to OCA! ");
+
+    }
+}
