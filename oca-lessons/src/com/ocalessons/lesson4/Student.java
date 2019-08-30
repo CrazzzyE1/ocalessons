0c07c31e28c481bc4ac3ad9d6db939b2eb074d6c
CrazzzyE1 <litvakivan@gmail.com>
2019-08-30 16:06:42 +0300

    Finish lesson 4

---
+++ oca-lessons/src/com/ocalessons/lesson4/Student.java
@@ -0,0 +1,11 @@
+package com.ocalessons.lesson4;
+
+public class Student {
+    int idNumber;
+    String name;
+    String Surname;
+    int year;
+    double avgMath;
+    double avgEconomy;
+    double avgEnglish;
+}
