60226d83bda912e91e78aa2a3865a1140783d10d
CrazzzyE1 <litvakivan@gmail.com>
2019-08-31 20:55:53 +0300

    Finish Home Task 6 of lesson

--- oca-lessons/src/com/ocalessons/Main.java
+++ oca-lessons/src/com/ocalessons/Main.java
@@ -6,6 +6,7 @@ import com.ocalessons.lesson3.MathAndLogitalOperation;
 import com.ocalessons.lesson4.ClassAndObjects;
 import com.ocalessons.lesson4.Student;
 import com.ocalessons.lesson5.ConstructorsAndMethods;
+import com.ocalessons.lesson6.OverloadedMethodsAndConstructors;
 
 public class Main {
 
@@ -13,16 +14,23 @@ public class Main {
 //        BasicPrintFunction basicPrintFunction = new BasicPrintFunction();
 //        PrimitiveDataType primitiveDataType = new PrimitiveDataType();
 //        MathAndLogitalOperation lesson3 = new MathAndLogitalOperation();
-        ClassAndObjects classAndObjects = new ClassAndObjects();
-        ConstructorsAndMethods constructorsAndMethods = new ConstructorsAndMethods();
+//        ClassAndObjects classAndObjects = new ClassAndObjects();
+//        ConstructorsAndMethods constructorsAndMethods = new ConstructorsAndMethods();
+        OverloadedMethodsAndConstructors omac = new OverloadedMethodsAndConstructors();
 
 //        basicPrintFunction.print();
 //        primitiveDataType.print();
 //        MathAndLogitalOperation.print();
-        classAndObjects.print();
-        constructorsAndMethods.start();
-        classAndObjects.print2();
-
+//        classAndObjects.print();
+//        constructorsAndMethods.start();
+//        classAndObjects.print2();
+        omac.summ();
+        omac.summ(5);
+        omac.summ(6, 7);
+        omac.summ(8,5,4,6);
+        omac.summ(8,6,4,9);
+        System.out.println();
+        omac.print();
 
 
     }
--- oca-lessons/src/com/ocalessons/lesson6/lesson6.java
+++
@@ -1,6 +0,0 @@
-package com.ocalessons.lesson6;
-
-public class lesson6 {
-
-    
-}
