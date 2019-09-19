bcd51e2a1933a659c54c1b66997b52a4ada59cb1
CrazzzyE1 <litvakivan@gmail.com>
2019-09-19 18:51:54 +0300

    finish

---
+++ oca-lessons/src/com/ocalessons/lesson12/Car.java
@@ -0,0 +1,13 @@
+package com.ocalessons.lesson12;
+
+public class Car {
+    public String color;
+    public String engine;
+    public int numberOfDoors;
+
+    public Car(String color, String engine, int numberOfDoors) {
+        this.color = color;
+        this.engine = engine;
+        this.numberOfDoors = numberOfDoors;
+    }
+}
