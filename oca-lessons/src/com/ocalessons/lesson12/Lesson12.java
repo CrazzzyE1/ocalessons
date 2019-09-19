17ea33093ee2f11156431172b8d59ebf53539635
CrazzzyE1 <litvakivan@gmail.com>
2019-09-19 18:51:47 +0300

    Finish lesson 12 Home task

--- oca-lessons/src/com/ocalessons/lesson12/Lesson12.java
+++ oca-lessons/src/com/ocalessons/lesson12/Lesson12.java
@@ -4,5 +4,34 @@ public class Lesson12 {
 
     public void startLesson12(){
         System.out.println("Start lesson 12");
+            Car car1 = new Car("Red", "V8", 5);
+            Car car2 = new Car("Red", "V8", 8);
+            equalMethod(car1, car2);
+            equalMethod2(car1, car2);
     }
+    public static void equalMethod(Car c1, Car c2) {
+        if(c1.color == c2.color && c1.engine == c2.engine && c1.numberOfDoors == c2.numberOfDoors) {
+            System.out.println("Машины одинаковые");
+        } else {
+            System.out.println("Машины разные");
+        }
+    }
+
+    public static void equalMethod2(Car c1, Car c2) {
+        if(c1.numberOfDoors == c2.numberOfDoors){
+            if(c1.engine == c2.engine) {
+                if(c1.color == c2.color) {
+                    System.out.println("EQUALS CARS");
+                } else {
+                    System.out.println("Color ERROR");
+                }
+
+            } else {
+                System.out.println("Engine ERROR");
+            }
+        } else {
+            System.out.println("Numbers of doors ERROR");
+        }
+    }
+
 }
