f4ef4340a1ab63e9783d170ff1c9c56192899b4d
CrazzzyE1 <litvakivan@gmail.com>
2019-10-04 22:42:04 +0300

    !

--- oca-lessons/src/com/ocalessons/lesson19/Lesson19.java
+++ oca-lessons/src/com/ocalessons/lesson19/Lesson19.java
@@ -2,6 +2,6 @@ package com.ocalessons.lesson19;
 
 public class Lesson19 {
     public void lesson19() {
-        System.out.println("Start lesson 19");
+        System.out.println("Start lesson 19!");
     }
 }
